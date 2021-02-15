82a6115e3c02c6576da601efaf66aadbd7c5f022
GoldenTails <milestailsprower101n2@gmail.com>
2021-02-14 18:54:24 -0600

    Improve buildMenu.sh further by including a build clean system, and telling you where it built.

--- buildMenu.sh
+++ buildMenu.sh
@@ -8,12 +8,32 @@ PK3_NAME=${PK3_NAME:-'menu'}
 
 PK3_FULLNAME="$PK3_FLAGS"_"$PK3_NAME"-"$PK3_VERSION"
 
+PK3_COMMIT=$(git rev-parse --short HEAD)
+
+if [[ "$*" == *"cleanbuilds"* ]]; then
+	shopt -s extglob
+
+	# Clean files that aren't .gitignore that contain +, but don't contain the current commit hash.
+	FILES_TO_RM=$(find builds -type f ! -name "*.gitignore*" -name "*+*" ! -name "*$PK3_COMMIT*")
+
+	if [ "$FILES_TO_RM" != "" ]; then
+		rm $FILES_TO_RM
+	fi
+
+	# Clean files that aren't .gitignore that don't match our current version.
+	FILES_TO_RM=$(find builds -type f ! -name "*.gitignore*" ! -name "*$PK3_VERSION*")
+
+	if [ "$FILES_TO_RM" != "" ]; then
+		rm $FILES_TO_RM
+	fi
+
+	exit # Don't create pk3
+fi
+
 if [[ ! $PK3_RELEASE ]]; then
-	PK3_COMMIT=$(git rev-parse --short HEAD)
 	PK3_TIME=$(date +"%m.%d.%y-%T")
 
 	PK3_METADATA="$PK3_COMMIT"_"$PK3_TIME"
-
 	PK3_FULLNAME="$PK3_FULLNAME"+"$PK3_METADATA"
 fi
 
@@ -30,3 +50,5 @@ zip -FSr ../builds/$PK3_FULLNAME.pk3 $(echo $PK3_FILES | tr '\r\n' ' ')
 
 cd ..
 ln -sf builds/$PK3_FULLNAME.pk3 $PK3_NAME.pk3
+
+echo "Build is located at builds/$PK3_FULLNAME.pk3"
